000d20beaf73793c0520471ff88f4e716ef0b2e5
Saurabh <saurabh.d2106@gmail.com>
2025-03-06 14:11:21 +0000

    Added Post Test case

--- tests/conduitapi.spec.ts
+++ tests/conduitapi.spec.ts
@@ -1,21 +1,57 @@
-import {expect, test} from "@playwright/test"
+import { expect, test } from "@playwright/test"
 
-test.describe("API Testing", ()=> {
+test.describe("API Testing", () => {
 
 
-    test("Get Tags API", async ({ request })=>{
+    test("Get Tags API", async ({ request }) => {
 
-       const response = await request.get("/api/tags", {})
+        const response = await request.get("/api/tags", {})
 
-       expect(response.ok()).toBeTruthy()
+        expect(response.ok()).toBeTruthy()
 
-       const responseData = await response.json()
+        const responseData = await response.json()
 
-       expect(responseData.tags[0]).toBe("selenium")
+        expect(responseData.tags[0]).toBe("selenium")
 
-       expect(responseData.tags[1]).toBe("test")
+        expect(responseData.tags[1]).toBe("test")
 
-       
+
+
+    })
+
+    test("Post API - Signin", async ({ request }) => {
+
+        const username = "test@test.com"
+        const password = "test"
+
+        const response = await request.post("/api/users/login", {
+            data:
+
+            {
+                "user": {
+                    "email": username,
+                    "password": password
+                }
+            }
+
+            ,
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            }
+        })
+
+        expect(response.ok()).toBeTruthy()
+
+        const responseData = await response.json();
+
+        expect(responseData.user.email).toBe(username)
+
+        const token = responseData.user.token
+
+        console.log(token)
 
     })
 })
